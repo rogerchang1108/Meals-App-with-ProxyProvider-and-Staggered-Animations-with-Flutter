e0affee93ac6bd944548ec76c8bb87060c03f9c0
rogerchang1108 <rogerchang1108@gmail.com>
2024-05-03 20:38:44 +0800

    feat: add staggered animation in HomeCategoriesTab

--- lib/widgets/home_categories_tab.dart
+++ lib/widgets/home_categories_tab.dart
@@ -6,8 +6,33 @@ import 'package:flutter_app/models/category.dart';
 import 'package:flutter_app/services/navigation.dart';
 import 'package:flutter_app/widgets/category_grid_item.dart';
 
-class HomeCategoriesTab extends StatelessWidget {
+class HomeCategoriesTab extends StatefulWidget {
   const HomeCategoriesTab({super.key});
+
+  @override
+  State<HomeCategoriesTab> createState() => _HomeCategoriesTabState();
+}
+
+class _HomeCategoriesTabState extends State<HomeCategoriesTab>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 3000),
+    );
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   void _selectCategory(BuildContext context, Category category) {
     final nav = Provider.of<NavigationService>(context, listen: false);
@@ -16,24 +41,80 @@ class HomeCategoriesTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      padding: const EdgeInsets.all(16),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 16,
-        mainAxisSpacing: 16,
+    return SlideTransition(
+      position: Tween(
+        begin: const Offset(0, 0.7),
+        end: const Offset(0, 0),
+      ).animate(
+        CurvedAnimation(
+          parent: _animationController,
+          curve: Curves.easeOutCubic,
+        ),
       ),
-      children: [
-        // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
-        for (final category in dummyCategories.values)
-          CategoryGridItem(
-            category: category,
-            onSelectCategory: () {
-              _selectCategory(context, category);
-            },
-          )
-      ],
+      child: GridView(
+        padding: const EdgeInsets.all(16),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 16,
+          mainAxisSpacing: 16,
+        ),
+        children: [
+          for (int i = 0; i < dummyCategories.values.length; i++)
+            StaggeredAnimationItem(
+              index: i,
+              animationController: _animationController,
+              child: CategoryGridItem(
+                category: dummyCategories.values.elementAt(i),
+                onSelectCategory: () {
+                  _selectCategory(context, dummyCategories.values.elementAt(i));
+                },
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
+
+class StaggeredAnimationItem extends StatelessWidget {
+  final int index;
+  final AnimationController animationController;
+  final Widget child;
+
+  const StaggeredAnimationItem({
+    super.key, 
+    required this.index,
+    required this.animationController,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    int rowIndex = index ~/ 2;
+    int totalRow = (dummyCategories.values.length+1)~/2;
+    // print(rowIndex);
+    // print(totalRow);
+    final double intervalStart = 0.5 / (totalRow-1) * rowIndex;
+    // print(intervalStart);
+    double intervalEnd = intervalStart + 0.5;
+    if(intervalEnd > 1.0){
+      intervalEnd = 1.0;
+    }
+    // print(intervalEnd);
+    final Interval interval = Interval(intervalStart, intervalEnd, curve: Curves.easeOutCubic);
+
+    return SlideTransition(
+      position: Tween<Offset>(
+        begin: Offset(0, (totalRow - rowIndex).toDouble()),
+        end: const Offset(0, 0),
+      ).animate(
+        CurvedAnimation(
+          parent: animationController,
+          curve: interval,
+        ),
+      ),
+      child: child,
     );
   }
 }
